6a06f1230e8241e06f2fbcf66cde67ea562a9ed6
madcamp_cjh <cjh0507@kaist.ac.kr>
2020-07-31 20:28:51 +0900

    카메라 무빙 추가

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,23 +5,36 @@ using UnityEngine;
 // 카메라가 플레이어를 따라다니게 해주는 스크립트
 public class CameraFollow : MonoBehaviour
 {
-    public float smoothTimex, smoothTimeY;
+    public float smoothTimeX, smoothTimeY;
     public Vector2 velocity;
-    public GameObject player;
-    public Vector2 minPos, maxPos;
-    public bool bound;
+	public GameObject player;
+	public Vector2 minPos, maxPos;
+	public bool bound;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag ("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
-    void LastUpdate()
+    void FixedUpdate()
     {
+        // Mathf.SmoothDamp는 천천히 값을 증가시키는 메서드이다.
         // Vector3.SmoothDamp(Vector3 current, Vector3 target, ref Vector3 Velocity, float smoothTime, float maxSpeed, float deltaTime);
-        // float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
+        // 카메라 이동
+		transform.position = new Vector3 (posX, posY, transform.position.z);
+
+		if(bound) {
+
+			//Mathf.Clamp(현재값, 최대값, 최소값);  현재값이 최대값까지만 반환해주고 최소값보다 작으면 그 최소값까지만 반환합니다.
+
+			transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minPos.x, maxPos.x),
+                Mathf.Clamp (transform.position.y, minPos.y, maxPos.y),
+                Mathf.Clamp (transform.position.z, transform.position.z, transform.position.z));
+        }
     }
 }
--- Assets/Scripts/Cells/Cell.cs
+++ Assets/Scripts/Cells/Cell.cs
@@ -5,35 +5,47 @@ using UnityEngine;
 // 모든 Cell들이 공통적으로 가져야할 특성에 대해서 정의하는 부분
 public class Cell : MonoBehaviour
 {
-    private List<Cell> adjacentCells; // Cell 혼자 있는 경우를 제외하면 1~6개의 원소를 가짐
+    private Cell[] adjacentCells = new Cell[6]; 
 
-    // Core cell로부터의 거리(Core의 coreDistance는 0이고 그 인접한 셀들의 coreDistance는 1인 식)
-    // Cell을 부착할 때 초기화해주어야 한다.
-    private int coreDistance; 
+    // // Core cell로부터의 거리(Core의 coreDistance는 0이고 그 인접한 셀들의 coreDistance는 1인 식)
+    // // Cell을 부착할 때 초기화해주어야 한다.
+    // private int coreDistance;
 
-    // 현재 adjacentCells에 기반하여 coreDistance를 설정해주는 함수
-    private void SetCoreDistance() {
-        if (adjacentCells == null || adjacentCells.Count == 0) {
-            // Cell 혼자인 경우
-            this.coreDistance = 0;
-            return;
-        }
-        // 맨 처음 CoreCell이 혼자일 때를 제외하면 모든 Cell은 adjacentCells에 적어도 하나의 원소가 있다.
-        int minDist = adjacentCells[0].coreDistance;
+    // // 현재 adjacentCells에 기반하여 coreDistance를 설정해주는 함수
+    // private void SetCoreDistance() {
+        
 
-        // 인접한 Cell들 중에서 coreDistance 최솟값 찾기
-        foreach (Cell cell in adjacentCells) {
-            int tempDist = cell.coreDistance; 
-            if (minDist > tempDist) {
-                minDist = tempDist;
-            }
-        }
+    //     // if (adjacentCells == null || adjacentCells.Count == 0) {
+    //     //     // Cell 혼자인 경우
+    //     //     this.coreDistance = 0;
+    //     //     return;
+    //     // }
+    //     // 맨 처음 CoreCell이 혼자일 때를 제외하면 모든 Cell은 adjacentCells에 적어도 하나의 원소가 있다.
 
-        // 최솟값에 +1 한 것이 이 Cell의 coreDistance
-        this.coreDistance = minDist + 1;
-    }
+    //     int minDist = adjacentCells[0].coreDistance;
 
-    public int GetCoreDistance() {
-        return coreDistance;
-    }
+    //     // 인접한 Cell들 중에서 coreDistance 최솟값 찾기
+    //     foreach (Cell cell in adjacentCells) {
+    //         int tempDist = cell.coreDistance; 
+    //         if (minDist > tempDist) {
+    //             minDist = tempDist;
+    //         }
+    //     }
+
+    //     // 최솟값에 +1 한 것이 이 Cell의 coreDistance
+    //     this.coreDistance = minDist + 1;
+    // }
+
+    // private bool isAlone() {
+
+    //     foreach ( Cell cell in adjacentCells) {
+
+    //     }
+
+    //     return true;
+    // }
+
+    // public int GetCoreDistance() {
+    //     return coreDistance;
+    // }
 }
