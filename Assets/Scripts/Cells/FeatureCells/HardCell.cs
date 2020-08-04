02c6975895acd8b6c0d023df3255ed0de51fbec8
madcamp_cjh <cjh0507@kaist.ac.kr>
2020-08-04 17:16:36 +0900

    HardCell 추가

--- Assets/Scripts/Cells/FeatureCells/HardCell.cs
+++ Assets/Scripts/Cells/FeatureCells/HardCell.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 // 인접 Cell들의 최대 내구도 증가(아이작 같은 식으로 증가한 내구도 만큼 회복)
 public class HardCell : FeatureCell
 {
+    float delta;
+    protected override void Start() {
+        // 본인도 단단함
+        maxDurability *= 1.3f;
+        durability = maxDurability;
+        base.Start();
+    }
+
     // 특정 인접 셀 하나에 Feature 부여 => FeatureCell에 다른 Cell이 붙었을 때
     // id : 0 ~ 5
     public override void GiveFeature(int id) {
-        
+        Cell targetCell = adjacentCells[id];
+        if(targetCell != null) {
+            // CoreCell은 조금 덜 늘어나게
+            if (targetCell.cellType == "CoreCell") {
+                delta = targetCell.maxDurability * 0.2f;
+            }
+            else {
+                delta = targetCell.maxDurability * 0.3f;
+            }
+            
+            // 최대체력이 늘어난 만큼 회복
+            targetCell.maxDurability += delta;
+            targetCell.durability += delta;
+            if (targetCell.durability > targetCell.maxDurability)
+                targetCell.durability = targetCell.maxDurability;
+        }
     }
 
     // 특정 인접 셀 하나의 Feature 취소 => FeatureCell에 붙은 다른 Cell이 떨어질 때(파괴될 때)
