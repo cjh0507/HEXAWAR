5e04fdb9d1c478bfb3ad74c3129fa2c3e39054e2
madcamp_cjh <cjh0507@kaist.ac.kr>
2020-08-05 23:04:56 +0900

    메인 메뉴/일시 정지 구현

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+
+    private bool paused = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseUI.SetActive(false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            paused = !paused;
+        }
+        if(paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        if(!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        
+    }
+    public void Resume() {
+        paused = !paused;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
